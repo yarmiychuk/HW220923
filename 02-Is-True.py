3836a6b1d6a3ac58fdf19e0cecbcd0de76cffebe
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-09-25 00:26:34 +0300

    Predicate app added

---
+++ 02-Is-True.py
@@ -0,0 +1,14 @@
+# Напишите программу для проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def isTrue(x, y, z):
+    if not x or not y or not z == (not x and not y and not z):
+        return 'ИСТИННО'
+    return 'ЛОЖНО'
+
+print('Проверка истинности утверждения ¬(X v Y v Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (x or y or z) == (not x and not y and not z):
+                print(f'При X = {x}, Y = {y}, Z = {z}) - {isTrue(x, y, z)}')
