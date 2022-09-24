951884edf554a349abc257c5abe41a04ac04847d
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-09-25 00:52:45 +0300

    Preticate app edited

--- 02-Is-True.py
+++ 02-Is-True.py
@@ -2,7 +2,7 @@
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
 def isTrue(x, y, z):
-    if not x or not y or not z == (not x and not y and not z):
+    if not (x or y or z) == (not x) and (not y) and (not z):
         return 'ИСТИННО'
     return 'ЛОЖНО'
 
@@ -10,5 +10,4 @@ print('Проверка истинности утверждения ¬(X v Y v Z
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            if not (x or y or z) == (not x and not y and not z):
-                print(f'При X = {x}, Y = {y}, Z = {z}) - {isTrue(x, y, z)}')
+            print(f'При X = {x}, Y = {y}, Z = {z}) - {isTrue(x, y, z)}')
